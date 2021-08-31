ebcacbf4f76ee338d99e863a75f0b3ca69c4a767
xiaolongchen0627 <52510143+xiaolongchen0627@users.noreply.github.com>
2021-08-31 17:51:26 -0500

    update2.addcnr

--- ChIP-seq/Shell scripts/pre.post.alignment.dm6.sh
+++ ChIP-seq/Shell scripts/pre.post.alignment.dm6.sh
@@ -1,10 +1,10 @@
 
 WORKDIR="/home/xchen2/SWISNF/Xiaolong/ChIP/"
-FASTQINPUT="/home/xchen2/SWISNF/ChIP/FASTQ/"
+FASTQINPUT="/home/xchen2/SWISNF/Xiaolong/ChIP/FASTQ/"
 
 
 ####### Trim adaptor
-TRIMMEDFQ="/home/xchen2/SWISNF/ChIP/FASTQ/TRIMMED"
+TRIMMEDFQ="/home/xchen2/SWISNF/Xiaolong/ChIP/FASTQ/TRIMMED"
 for i in `ls $FASTQINPUT/*R1*.gz ` ;do bsub -J trimedfq trim_galore --paired --fastqc --gzip $i ${i/R1/R2} ; done 
 
 ####### Map to DM6 genome to calculated spike-in 
@@ -13,7 +13,6 @@ cd $TRIMMEDFQ
 for R1 in *.R1.fq.gz ;do 
 sampleName=${i/.R1.fq.gz};
 R2=${R1/R1/R2};
-mm10=$(cat /home/xchen2/SWISNF/Xiaolong/chrs.mm10.lst |tr "\n" " "); 
 echo "
 bowtie2 -x /rgs01/applications/hpcf/authorized_apps/cab/Automation/REF/Drosophila_melanogaster/Ensembl/r97/bowtie2-index/v2.3.5.1/Drosophila_melanogaster.BDGP6.22.dna.toplevel -1 $R1 -2 $R2 -S $sampleName.dm6.sam ; samtools view -Sb -F 4 $sampleName.dm6.sam > $sampleName.dm6.bam 
 samtools sort $sampleName.dm6.bam $sampleName.dm6.sorted
